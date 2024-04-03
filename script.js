2c65c912b76767139ec5d34c74c6a3dedb6b49df
leocodes-al <leonardo.sistemainformacao@gmail.com>
2024-04-03 12:52:30 -0300

    Updates

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  /*
-  if(html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  }
-  O evento a cima vai verificar se o html vai está no modo light, se estiver ele remove e vai para o modo escuro, caso nao esteja no modo light, ele add o modo light -- JA O CODIGO ABAIXO, faz o mesmo papel do de cima e é muito mas facil*/
-
   html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
